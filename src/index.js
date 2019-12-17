4ae2ff231f93d41ea8862c59e09c07f0a1050618
talita <polikashina@gmail.com>
2019-12-17 16:07:39 +0100

    step 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-	console.log();
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
+  console.log();
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default greeting;
-
-
