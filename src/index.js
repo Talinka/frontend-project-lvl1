9b3d0bc613d6e9029ad58858f43c1c7d2eb9158c
talita <polikashina@gmail.com>
2019-12-17 21:38:14 +0100

    step 5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+const greeting = (text) => {
+  console.log('Welcome to the Brain Games!');
+  if (text) {
+    console.log(text);
+  }
   console.log();
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export default greeting;
+const evenGame = () => {
+  const text = 'Answer "yes" if the number is even, otherwise answer "no"';
+  const userName = greeting(text);
+  console.log();
+
+  for (let i = 0; i < 3; i += 1) {
+    const num = Math.round(Math.random() * 100);
+    const rigthAnswer = num % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${num} `);
+    const answer = readlineSync.question('Your answer: ').toLowerCase();
+
+    if (answer !== rigthAnswer) {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${rigthAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { greeting, evenGame };
